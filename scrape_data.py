a7bdc7e19af91584512883c00783401e092b4107
Jamie <jmaslek11@gmail.com>
2021-07-07 22:01:33 -0400

    update script

--- scrape_data.py
+++ scrape_data.py
@@ -3,16 +3,18 @@ import pandas as pd
 from bs4 import BeautifulSoup as bs
 import numpy as np
 
+
 def assets_to_num(x):
     x = x.strip("$")
     if x.endswith("M"):
         return float(x.strip("M"))
     elif x.endswith("B"):
-        return float(x.strip("B"))*1000
+        return float(x.strip("B")) * 1000
     elif x.endswith("K"):
-        return float(x.strip("K"))/1000
+        return float(x.strip("K")) / 1000
     else:
         return np.nan
+
 
 r = requests.get("https://stockanalysis.com/etf/")
 soup = bs(r.text, "html.parser").findAll("ul", {"class": "no-spacing"})
@@ -22,10 +24,8 @@ etf_symbols = []
 for link in all_links:
     etf_symbols.append(link.text.split("-")[0].strip(" "))
 
-df = pd.DataFrame(columns = etf_symbols)
-for counter,etf in enumerate(etf_symbols):
-    if counter%100 == 0:
-        print(f"{counter}/{len(etf_symbols)}")
+df = pd.DataFrame(columns=etf_symbols)
+for etf in etf_symbols:
     r1 = requests.get(f"https://stockanalysis.com/etf/{etf}")
     soup1 = bs(r1.text, "html.parser").find("div", {"class": "info"}).findAll("td")
     column = []
@@ -47,9 +47,13 @@ df = df.T
 df.columns = ["Price", "Assets", "NAV", "Expense", "PE", "Beta", "Div", "DivYield"]
 df["Price"] = df["Price"].apply(lambda x: float(x.strip("$")))
 df["NAV"] = df["NAV"].apply(lambda x: float(x.strip("$")) if x != "n/a" else np.nan)
-df["Expense"] = df["Expense"].apply(lambda x: float(x.strip("%")) if x != "n/a" else np.nan)
+df["Expense"] = df["Expense"].apply(
+    lambda x: float(x.strip("%")) if x != "n/a" else np.nan
+)
 df["Div"] = df["Div"].apply(lambda x: float(x.strip("$")) if x != "n/a" else np.nan)
-df["DivYield"] = df["DivYield"].apply(lambda x: float(x.strip("%")) if x != "n/a" else np.nan)
+df["DivYield"] = df["DivYield"].apply(
+    lambda x: float(x.strip("%")) if x != "n/a" else np.nan
+)
 df["PE"] = df["PE"].apply(lambda x: float(x) if x != "n/a" else np.nan)
 df["Beta"] = df["Beta"].apply(lambda x: float(x) if x != "n/a" else np.nan)
 df["Assets"] = df["Assets"].apply(lambda x: assets_to_num(x))
