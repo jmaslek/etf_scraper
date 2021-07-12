3433f433d4f944223f71ee2f32173c07b952ba0a
jmaslek <jmaslek11@gmail.com>
2021-07-12 13:15:31 -0400

    Update scrape_holdings.py

--- scrape_holdings.py
+++ scrape_holdings.py
@@ -13,38 +13,41 @@ for link in all_links:
     etf_symbols.append(link.text.split("-")[0].strip(" "))
 
 for etf in etf_symbols:
-    file = f"data/{etf}.csv"
-    r1 = requests.get(f"https://stockanalysis.com/etf/{etf}/holdings")
-    s1 = (
-        bs(r1.text, "html.parser")
-            .find("table", {"class": "fullholdings"})
-            .find("tbody")
-    )
-    tick, percent, shares = [], [], []
-    for idx, entry in enumerate(s1.findAll("tr"), 1):
-        if entry.findAll("td")[1].text == "n/a":
-            tick.append(entry.findAll("td")[2].text)
-        else:
-            tick.append(entry.findAll("td")[1].text)
-        percent.append(float(entry.findAll("td")[3].text.strip("%").replace(",","_")))
-        if idx >= 200:
-            break
-    df = pd.DataFrame(data=[percent])
-    df.columns = tick
-    df.index = [datetime.datetime.now().strftime("%m/%d/%Y")]
-    df["SUM"] = df.sum(axis=1)
-    if df.columns.value_counts().max() > 1:
-        # Seems to occur for me with symbol 1117.hk having many desciptions as an example
-        continue
-
     try:
-        df_historical = pd.read_csv(file, index_col=0)
-        new_df = pd.concat([df_historical, df], axis=0).fillna(0)
-    except FileNotFoundError:
-        new_df = df.copy()
-    # Make SUM the last column
-    ordered_cols = list(new_df.columns)
-    ordered_cols.remove("SUM")
-    ordered_cols += ["SUM"]
-    new_df = new_df[ordered_cols]
-    new_df.to_csv(file)
+        file = f"data/{etf}.csv"
+        r1 = requests.get(f"https://stockanalysis.com/etf/{etf}/holdings")
+        s1 = (
+            bs(r1.text, "html.parser")
+                .find("table", {"class": "fullholdings"})
+                .find("tbody")
+        )
+        tick, percent, shares = [], [], []
+        for idx, entry in enumerate(s1.findAll("tr"), 1):
+            if entry.findAll("td")[1].text == "n/a":
+                tick.append(entry.findAll("td")[2].text)
+            else:
+                tick.append(entry.findAll("td")[1].text)
+            percent.append(float(entry.findAll("td")[3].text.strip("%").replace(",","_")))
+            if idx >= 200:
+                break
+        df = pd.DataFrame(data=[percent])
+        df.columns = tick
+        df.index = [datetime.datetime.now().strftime("%m/%d/%Y")]
+        df["SUM"] = df.sum(axis=1)
+        if df.columns.value_counts().max() > 1:
+            # Seems to occur for me with symbol 1117.hk having many desciptions as an example
+            continue
+
+        try:
+            df_historical = pd.read_csv(file, index_col=0)
+            new_df = pd.concat([df_historical, df], axis=0).fillna(0)
+        except FileNotFoundError:
+            new_df = df.copy()
+        # Make SUM the last column
+        ordered_cols = list(new_df.columns)
+        ordered_cols.remove("SUM")
+        ordered_cols += ["SUM"]
+        new_df = new_df[ordered_cols]
+        new_df.to_csv(file)
+    except Exception as e:
+        pass
