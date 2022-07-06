a8fdb6bbc7e00b5d083e96599ddf6f59b02b0ed0
James Maslek <jmaslek11@gmail.com>
2022-07-06 14:45:01 -0400

    Update scrape_data.py

--- scrape_data.py
+++ scrape_data.py
@@ -21,7 +21,7 @@ def assets_to_num(x):
     
 s = requests.Session()
 
-retries = Retry(total=10,
+retries = Retry(total=5,
                 backoff_factor=.5,
                 status_forcelist=[ 429 ])
 
@@ -35,7 +35,7 @@ a1 = json.loads(soup2.find_all("script")[4].string)
 etf_symbols = pd.DataFrame(a1["data"]).s.to_list()
     
 df = pd.read_csv("etf_overviews.csv", index_col=0).T
-for etf in etf_symbols[:750]:
+for etf in etf_symbols[:500]:
     try:
         #r = requests.get(f"https://stockanalysis.com/etf/{etf}", headers={"User-Agent":"Mozilla/5.0"})
         r = s.get(f"https://stockanalysis.com/etf/{etf}", headers={"User-Agent":"Mozilla/5.0"})
